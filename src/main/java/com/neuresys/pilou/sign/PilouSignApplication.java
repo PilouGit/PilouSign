ceb0f1cc18bcef1574335725c1395db345775876
PilouGit <pierre-emmanuel.gros@neuresys.fr>
2022-06-17 15:59:16 +0200

    first commit

---
+++ src/main/java/com/neuresys/pilou/sign/PilouSignApplication.java
@@ -0,0 +1,13 @@
+package com.neuresys.pilou.sign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilouSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilouSignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neuresys/pilou/sign/PilouSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuresys.pilou.sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilouSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
